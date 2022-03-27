28b417ff79c237ab66daf28e4310a67774eb1c60
snsakib <hello@snsakib.com>
2022-03-27 08:50:10 +0600

    setup mongodb

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 
+const Event = require('./models/event');
+
 const app = express();
-
-const events = [
-
-];
 
 app.use(bodyParser.json());
 
@@ -44,24 +42,40 @@ app.use('/graphql', graphqlHTTP({
   `),
   rootValue: {
     events: () => {
-      return events;
+      return Event.find()
+        .then(events => {
+          return events.map(event => {
+            return {...event._doc};
+          });
+        })
+        .catch(err => {
+          throw err;
+        })
     },
     createEvent: (args) => {
-      const event = {
-        _id: Math.random().toString(),
+      const event = new Event({
         title: args.eventInput.title,
         description: args.eventInput.description,
         price: +args.eventInput.price,
-        date: args.eventInput.date
-      }
-      events.push(event);
-      return event;
+        date: new Date(args.eventInput.date)
+      });
+
+      return event
+        .save()
+        .then(result => {
+          console.log(result);
+          return {...result._doc};
+        })
+        .catch(err => {
+          console.log(err);
+          throw err;
+        });
     }
   },
   graphiql: true
 }));
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.oyvdd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.oyvdd.mongodb.net/${process.env.MONGO_DATABASE_NAME}?retryWrites=true&w=majority`)
   .then(() => {
     app.listen(3000);
   })
